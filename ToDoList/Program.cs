de202fbe665f0f54bdbbba2370c7eea6798c8f71
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-10 19:14:50 -0700

    Update project with multiple controllers and views

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,40 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
-using System.Drawing;
-using Console = Colorful.Console;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace UserInterface
+namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-
-      Console.WriteLine("Welcome to your to-do list.");
-      while (true)
-      {
-        Console.WriteLine("Would you like to add a new item to your list, or view all current items? Add Item/View List");
-        string userInput = Console.ReadLine();
-        string userAnswer = userInput.ToLower();
-        if (userAnswer == "add item")
-        {
-          Console.WriteLine("Great! What would you like to add?");
-          string description1 = Console.ReadLine();
-          Item newItem = new Item(description1);
-          List<Item> newList = new List<Item> { newItem };
-          Console.WriteLine(description1 + " has been added to your list.");
-        }
-        else
-        {
-          List<Item> result = Item.GetAll();
-          foreach (Item individualItem in result)
-          {
-            Console.WriteLine(individualItem.Description);
-          }
-        }
-      }
+      host.Run();
     }
   }
 }
