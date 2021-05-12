23dc814eb96989ca6f58b577035ce5b33dd0526a
Ryan Walker <ryandanielwalker@gmail.com>
2021-05-12 11:11:14 -0700

    Add Find Id functionality

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
   public class Item
   {
     public string Description { get; set; }
+    public int Id { get; }
     public int Priority { get; set; }
     private static List<Item> _instances = new List<Item> { };
 
@@ -12,6 +13,7 @@ namespace ToDoList.Models
     {
       Description = description;
       _instances.Add(this);
+      Id = _instances.Count;
     }
 
     public Item(string description, int priority)
@@ -28,6 +30,10 @@ namespace ToDoList.Models
     public static void ClearAll()
     {
       _instances.Clear();
+    }
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId - 1];
     }
   }
 }
